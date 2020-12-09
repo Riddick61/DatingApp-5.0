2965e253882adfa793d1fc2cd35b0f52061f9e0a
Riddick61 <d.mustara@gmail.com>
2020-12-09 16:52:45 +0100

    Added Extension method section 4 - Token and Ident

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -6,20 +6,23 @@ using API.Entities;
 using Microsoft.AspNetCore.Mvc;
 using API.DTOs;
 using Microsoft.EntityFrameworkCore;
+using API.Interfaces;
 
 namespace API.Controllers
 {
     public class AccountController:BaseApiController
     {
         private readonly DataContext _context;
+        private readonly ITokenService _tokenService;
 
-        public AccountController(DataContext context)
+        public AccountController(DataContext context, ITokenService tokenService)
         {
             _context = context;
+            _tokenService = tokenService;
         }
 
         [HttpPost("register")]
-        public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto )
+        public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto )
         {
             if(await UserExists(registerDto.UserName)) return BadRequest("Username is taken");
             using var hmac = new HMACSHA512();
@@ -32,11 +35,15 @@ namespace API.Controllers
 
             _context.Add(user);
             await _context.SaveChangesAsync();
-            return user;
+            return new UserDto
+            {
+                Username = user.UserName,
+                Token = _tokenService.CreateToken(user)
+            };
         }
 
         [HttpPost("login")]
-        public async Task<ActionResult<AppUser>> Login(LoginDto loginDto )
+        public async Task<ActionResult<UserDto>> Login(LoginDto loginDto )
         {
             var user = await _context.Users.SingleOrDefaultAsync(x=> x.UserName == loginDto.Username);
             if (user == null) return Unauthorized("Invalid username");
@@ -49,7 +56,11 @@ namespace API.Controllers
                 if(computedHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid password");
             }
 
-            return user;
+            return new UserDto
+            {
+                Username = user.UserName,
+                Token = _tokenService.CreateToken(user)
+            };
         }
 
         private async Task<bool> UserExists(string username)
--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,5 +1,6 @@
 ﻿using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -20,12 +21,14 @@ namespace API.Controllers
         }
 
         [HttpGet]
+        [AllowAnonymous]
         public async Task<IEnumerable<AppUser>> GetUsers()
         {
             return await _context.Users.ToListAsync();
         }
 
         [HttpGet("{id}")]
+        [Authorize]
         public async Task<AppUser> GetUsers(int id)
         {
             return await _context.Users.FindAsync(id);
